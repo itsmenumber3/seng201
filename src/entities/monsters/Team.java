9ba3c6d58427ded2f7f4d7c736e69f7e9259feab
Francis Phan <hph44@uclive.ac.nz>
2022-04-24 19:06:15 +1200

    Various things

--- src/entities/monsters/Team.java
+++ src/entities/monsters/Team.java
@@ -8,14 +8,29 @@ import main.Role;
 import java.util.ArrayList;
 
 public class Team implements Role {
-    private MagicNumbers magicNumbers = new MagicNumbers();
-    public ArrayList<Monster> team = new ArrayList<>();
+
+    // LIBRARIES ---------------------------------------------------
+
+    private final MagicNumbers magicNumbers = new MagicNumbers();
+
+    // LIBRARIES ---------------------------------------------------
 
     @Override
     public RoleType getRoleType() {
         return RoleType.TEAM;
     }
 
+    // TEAM OF MONSTERS --------------------------------------------
+
+    /**
+     * This array list will store the team of monsters.
+     */
+    public ArrayList<Monster> team = new ArrayList<>();
+
+    /**
+     * This method will add the input monster object to this team.
+     * @param inputMonster Monster, the new monster in question
+     */
     public void addMonsterToTeam(Monster inputMonster) {
         try {
             if (team.size() == magicNumbers.MAXIMUM_MONSTERS_NUMBER_IN_TEAM) {
@@ -28,7 +43,13 @@ public class Team implements Role {
         }
     }
 
+    /**
+     * This method will add the input monster
+     * @param inputMonster
+     */
     public void removeMonsterFromTeam(Monster inputMonster) {
         this.team.remove(inputMonster);
     }
+
+    // TEAM OF MONSTERS --------------------------------------------
 }
