8ba279ee1e1ed13b87c994baa4367f10edd190fc
Jonathan Nicholas <jni68@uclive.ac.nz>
2022-05-19 00:06:57 +1200

    PSR and CoinFlip ui screens

---
+++ src/ui/FlipCoinScreen.java
@@ -0,0 +1,91 @@
+package ui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.ImageIcon;
+import javax.swing.JToggleButton;
+import javax.swing.JButton;
+
+public class FlipCoinScreen {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FlipCoinScreen window = new FlipCoinScreen();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public FlipCoinScreen() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 750, 500);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblFlipACoin = new JLabel("Flip a Coin!");
+		lblFlipACoin.setHorizontalAlignment(SwingConstants.CENTER);
+		lblFlipACoin.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		lblFlipACoin.setBounds(197, 11, 356, 49);
+		frame.getContentPane().add(lblFlipACoin);
+		
+		JLabel lblselectHeadsOr = new JLabel("<html><div>Select Heads or Tails below to see if you win this chance to attack</div></html>");
+		lblselectHeadsOr.setHorizontalAlignment(SwingConstants.CENTER);
+		lblselectHeadsOr.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		lblselectHeadsOr.setBounds(72, 71, 606, 90);
+		frame.getContentPane().add(lblselectHeadsOr);
+		
+		JLabel lblTailSymbol = new JLabel("");
+		lblTailSymbol.setIcon(new ImageIcon(FlipCoinScreen.class.getResource("/assets/ui/img/Tails.jpg")));
+		lblTailSymbol.setBounds(202, 194, 96, 96);
+		frame.getContentPane().add(lblTailSymbol);
+		
+		JLabel lblHeadsSymbol = new JLabel("");
+		lblHeadsSymbol.setIcon(new ImageIcon(FlipCoinScreen.class.getResource("/assets/ui/img/Heads.jpg")));
+		lblHeadsSymbol.setBounds(452, 194, 96, 96);
+		frame.getContentPane().add(lblHeadsSymbol);
+		
+		JToggleButton tglbtnHeads = new JToggleButton("Heads");
+		tglbtnHeads.setBounds(452, 315, 96, 23);
+		frame.getContentPane().add(tglbtnHeads);
+		
+		JToggleButton tglbtnTails = new JToggleButton("Tails");
+		tglbtnTails.setBounds(202, 315, 96, 23);
+		frame.getContentPane().add(tglbtnTails);
+		
+		JLabel lblSelectionDisplay = new JLabel("You've selected: <choice>");
+		lblSelectionDisplay.setHorizontalAlignment(SwingConstants.CENTER);
+		lblSelectionDisplay.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblSelectionDisplay.setBounds(248, 356, 204, 33);
+		frame.getContentPane().add(lblSelectionDisplay);
+		
+		JButton btnFlipCoin = new JButton("Flip the Coin");
+		btnFlipCoin.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		btnFlipCoin.setBounds(270, 400, 160, 42);
+		frame.getContentPane().add(btnFlipCoin);
+	}
+
+}
--- src/ui/RockPaperScissorsScreen.java
+++ src/ui/RockPaperScissorsScreen.java
@@ -3,6 +3,13 @@ package ui;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JList;
+import javax.swing.JToggleButton;
 
 public class RockPaperScissorsScreen {
 
@@ -38,6 +45,50 @@ public class RockPaperScissorsScreen {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 750, 500);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblTitle = new JLabel("Paper, Scissors, Rock");
+		lblTitle.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		lblTitle.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTitle.setBounds(197, 11, 356, 49);
+		frame.getContentPane().add(lblTitle);
+		
+		JLabel lblDescription = new JLabel("<html><div>Select Paper, Scissors or Rock below to see if you win this chance to attack</div></html>");
+		lblDescription.setHorizontalAlignment(SwingConstants.CENTER);
+		lblDescription.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		lblDescription.setBounds(72, 71, 606, 112);
+		frame.getContentPane().add(lblDescription);
+		
+		JLabel lblPaperSymbol = new JLabel("");
+		lblPaperSymbol.setIcon(new ImageIcon(RockPaperScissorsScreen.class.getResource("/assets/ui/img/paper.png")));
+		lblPaperSymbol.setBounds(102, 220, 96, 96);
+		frame.getContentPane().add(lblPaperSymbol);
+		
+		JLabel lblScissorsSymbol = new JLabel("");
+		lblScissorsSymbol.setIcon(new ImageIcon(RockPaperScissorsScreen.class.getResource("/assets/ui/img/scissors.png")));
+		lblScissorsSymbol.setBounds(327, 220, 96, 96);
+		frame.getContentPane().add(lblScissorsSymbol);
+		
+		JLabel lblRockSymbol = new JLabel("");
+		lblRockSymbol.setIcon(new ImageIcon(RockPaperScissorsScreen.class.getResource("/assets/ui/img/rock.png")));
+		lblRockSymbol.setBounds(552, 220, 96, 96);
+		frame.getContentPane().add(lblRockSymbol);
+		
+		JButton btnNewButton = new JButton("Confirm Choice");
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		btnNewButton.setBounds(303, 400, 144, 49);
+		frame.getContentPane().add(btnNewButton);
+		
+		JToggleButton tglbtnPaper = new JToggleButton("Paper");
+		tglbtnPaper.setBounds(102, 330, 96, 23);
+		frame.getContentPane().add(tglbtnPaper);
+		
+		JToggleButton tglbtnScissors = new JToggleButton("Scissors");
+		tglbtnScissors.setBounds(327, 330, 96, 23);
+		frame.getContentPane().add(tglbtnScissors);
+		
+		JToggleButton tglbtnRock = new JToggleButton("Rock");
+		tglbtnRock.setBounds(552, 330, 96, 23);
+		frame.getContentPane().add(tglbtnRock);
 	}
-
 }
