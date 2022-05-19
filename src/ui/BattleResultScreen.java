17414eda60333c4ce129dacd90d42c9b4742b752
Jonathan Nicholas <jni68@uclive.ac.nz>
2022-05-19 14:24:47 +1200

    battle_result and challenge_result screen

---
+++ src/ui/BattleResultScreen.java
@@ -0,0 +1,98 @@
+package ui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.SystemColor;
+import javax.swing.JButton;
+
+public class BattleResultScreen {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					BattleResultScreen window = new BattleResultScreen();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public BattleResultScreen() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.getContentPane().setBackground(SystemColor.info);
+		frame.setBounds(100, 100, 750, 500);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblTitle = new JLabel("You ... <dynamic> the battle!");
+		lblTitle.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTitle.setFont(new Font("Tahoma", Font.PLAIN, 19));
+		lblTitle.setBounds(175, 11, 350, 35);
+		frame.getContentPane().add(lblTitle);
+		
+		JPanel WinPanel = new JPanel();
+		WinPanel.setBackground(new Color(0, 51, 153));
+		WinPanel.setBounds(59, 111, 267, 323);
+		frame.getContentPane().add(WinPanel);
+		WinPanel.setLayout(null);
+		
+		JLabel lblBeatTheMonster = new JLabel("You beat the <battle_monster>");
+		lblBeatTheMonster.setHorizontalAlignment(SwingConstants.CENTER);
+		lblBeatTheMonster.setBackground(SystemColor.info);
+		lblBeatTheMonster.setBounds(31, 11, 226, 21);
+		WinPanel.add(lblBeatTheMonster);
+		
+		JLabel lblMonsterReward = new JLabel("<html><div>You've been given $/battle_monster.sell_price/ as a reward from the NZ Government</div></html>");
+		lblMonsterReward.setBackground(SystemColor.info);
+		lblMonsterReward.setBounds(56, 63, 137, 87);
+		WinPanel.add(lblMonsterReward);
+		
+		JButton btnBackToMap = new JButton("Back to Map");
+		btnBackToMap.setBounds(69, 269, 124, 23);
+		WinPanel.add(btnBackToMap);
+		
+		JPanel LosePanel = new JPanel();
+		LosePanel.setBackground(new Color(0, 51, 153));
+		LosePanel.setBounds(377, 111, 267, 323);
+		frame.getContentPane().add(LosePanel);
+		LosePanel.setLayout(null);
+		
+		JLabel lblLostToTheMonster = new JLabel("You Lost to the <battle_monster>");
+		lblLostToTheMonster.setBounds(62, 37, 167, 14);
+		LosePanel.add(lblLostToTheMonster);
+		
+		JLabel lblNewLabel = new JLabel("Noooooo........");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(62, 79, 99, 37);
+		LosePanel.add(lblNewLabel);
+		
+		JButton btnBackToMap1 = new JButton("Back to Map");
+		btnBackToMap1.setBounds(77, 267, 123, 23);
+		LosePanel.add(btnBackToMap1);
+	}
+	
+}
---
+++ src/ui/ChallengeResultScreen.java
@@ -0,0 +1,112 @@
+package ui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+
+public class ChallengeResultScreen {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ChallengeResultScreen window = new ChallengeResultScreen();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public ChallengeResultScreen() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 750, 500);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("You <win_or_lose> the challenge!");
+		lblNewLabel.setBounds(190, 22, 320, 47);
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JPanel panel_Team = new JPanel();
+		panel_Team.setBounds(62, 122, 244, 272);
+		frame.getContentPane().add(panel_Team);
+		panel_Team.setLayout(null);
+		
+		JLabel lblTeamTitle = new JLabel("Your team");
+		lblTeamTitle.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTeamTitle.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblTeamTitle.setBounds(62, 11, 109, 26);
+		panel_Team.add(lblTeamTitle);
+		
+		JLabel lblMonster1Health = new JLabel("<name> current health : <healthLevel>");
+		lblMonster1Health.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblMonster1Health.setBounds(10, 45, 224, 26);
+		panel_Team.add(lblMonster1Health);
+		
+		JLabel lblMonster2Health = new JLabel("<name> current health : <healthLevel>");
+		lblMonster2Health.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblMonster2Health.setBounds(10, 105, 224, 26);
+		panel_Team.add(lblMonster2Health);
+		
+		JLabel lblMonster3Health = new JLabel("<name> current health : <healthLevel>");
+		lblMonster3Health.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblMonster3Health.setBounds(10, 164, 224, 26);
+		panel_Team.add(lblMonster3Health);
+		
+		JLabel lblMonster4Health = new JLabel("<name> current health : <healthLevel>");
+		lblMonster4Health.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblMonster4Health.setBounds(10, 223, 224, 26);
+		panel_Team.add(lblMonster4Health);
+		
+		JPanel panel_Boss = new JPanel();
+		panel_Boss.setBounds(377, 122, 244, 272);
+		frame.getContentPane().add(panel_Boss);
+		panel_Boss.setLayout(null);
+		
+		JLabel lblBossTitle = new JLabel("<location> boss");
+		lblBossTitle.setHorizontalAlignment(SwingConstants.CENTER);
+		lblBossTitle.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblBossTitle.setBounds(57, 11, 136, 39);
+		panel_Boss.add(lblBossTitle);
+		
+		JLabel lblNewLabel_1 = new JLabel("Current health : <healthLevel>");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblNewLabel_1.setBounds(10, 61, 224, 68);
+		panel_Boss.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("<html><div><p>The monster is</p>\r\n<p>angry if health >80</p>\r\n<p>annoyed if health <80 and >30</p>\r\n<p>sleepy if health <30</p></div></html>");
+		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNewLabel_2.setBounds(20, 129, 201, 99);
+		panel_Boss.add(lblNewLabel_2);
+		
+		JButton btnContinueBattle = new JButton("Continue Battle");
+		btnContinueBattle.setBounds(266, 405, 184, 37);
+		frame.getContentPane().add(btnContinueBattle);
+	}
+
+}
